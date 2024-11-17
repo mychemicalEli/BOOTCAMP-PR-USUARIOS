85d55ce088491adba3fd5c9dbd8e377fc6766ad2
mychemicalEli <elizabethblancomendez97@gmail.com>
2024-11-17 19:24:57 +0100

    stablished initial config

--- frontend/users-frontend/src/app/app.module.ts
+++ frontend/users-frontend/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './layouts/navbar/navbar.component';
+import { FooterComponent } from './layouts/footer/footer.component';
+import { UserFormComponent } from './users/user-form/user-form.component';
+import { UserListComponent } from './users/user-list/user-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserListComponent,
+    NavbarComponent,
+    FooterComponent,
+    UserFormComponent
   ],
   imports: [
     BrowserModule,
